d3d7803f0e4a17721490f031d0f479e390974d9a
calipsso <kamil.vizy@gmail.com>
2024-04-02 16:24:16 +0200

    updt

--- Model.py
+++ Model.py
@@ -5,11 +5,11 @@ import psycopg2
 class DbConnection:
     def __init__(self):
         self.conn = psycopg2.connect(
-            dbname="b81s1xhecmfxnpmteb27",
-            user="urici3i0icy8cuufham5",
-            password="FrtbL6FqnCsROZMWocDBkgoOeoZC7R",
-            host="b81s1xhecmfxnpmteb27-postgresql.services.clever-cloud.com",
-            port="50013"
+            dbname="xx",
+            user="xx",
+            password="xx",
+            host="xx",
+            port="xx"
         )
         self.cursor = self.conn.cursor()
     def ExecQuery(self, query):
@@ -24,13 +24,13 @@ class DbLibraryOperations:
         self.db_connection = DbConnection()
 
     def addBook(self, bookInfo):
-        add = ("insert into books (title, author_id, genre_id, ISBN, publication_year, copies) VALUES (%s, %s, %s, %s, %s, %s, %s)",(bookInfo))
+        add = ("INSERT INTO books (title, author_id, genre_id, ISBN, publication_year, copies) VALUES (%s, %s, %s, %s, %s, %s, %s)", (bookInfo, ))
         return self.db_connection.ExecQuery(add)
     def searchBook(self, searchInfo):
-        search = ("SELECT title, author_id, genre_id from books WHERE title = %s", (searchInfo))
+        search = ("SELECT title, author_id, genre_id from books WHERE title = %s", (searchInfo, ))
         return self.db_connection.ExecQuery(search)
     def addUser(self, userInfo):
-        addUsr = ("INSERT INTO members (first_name, last_name, email, registration_date) VALUES (%s, %s, %s, %s)",(userInfo))
+        addUsr = ("INSERT INTO members (first_name, last_name, email, registration_date) VALUES (%s, %s, %s, %s)",(userInfo, ))
         return self.db_connection.ExecQuery(addUsr)
 
     def checkUsr(self, checkInfo):
