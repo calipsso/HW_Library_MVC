dec6c43af3532a5c498eb5205138bc983946a054
calipsso <kamil.vizy@gmail.com>
2024-04-02 20:07:15 +0200

    from class

--- Model.py
+++ Model.py
@@ -5,15 +5,15 @@ import psycopg2
 class DbConnection:
     def __init__(self):
         self.conn = psycopg2.connect(
-            dbname="xx",
-            user="xx",
-            password="xx",
-            host="xx",
-            port="xx"
+            dbname="b81s1xhecmfxnpmteb27",
+            user="urici3i0icy8cuufham5",
+            password="FrtbL6FqnCsROZMWocDBkgoOeoZC7R",
+            host="b81s1xhecmfxnpmteb27-postgresql.services.clever-cloud.com",
+            port="50013"
         )
         self.cursor = self.conn.cursor()
-    def ExecQuery(self, query):
-        self.cursor.execute(query)
+    def ExecQuery(self, query, parameters):
+        self.cursor.execute(query, parameters)
         return self.cursor.fetchall()
 
     def CloseConnection(self):
@@ -24,8 +24,8 @@ class DbLibraryOperations:
         self.db_connection = DbConnection()
 
     def addBook(self, bookInfo):
-        add = ("INSERT INTO books (title, author_id, genre_id, ISBN, publication_year, copies) VALUES (%s, %s, %s, %s, %s, %s, %s)", (bookInfo, ))
-        return self.db_connection.ExecQuery(add)
+        add = "INSERT INTO books (title, author_id, genre_id, ISBN, publication_year, copies) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        return self.db_connection.ExecQuery(add, (bookInfo, ))
     def searchBook(self, searchInfo):
         search = ("SELECT title, author_id, genre_id from books WHERE title = %s", (searchInfo, ))
         return self.db_connection.ExecQuery(search)
