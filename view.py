88cd0e9e71ca2f2cbdd4bd68d02cc51e978db8c8
calipsso <kamil.vizy@gmail.com>
2024-04-03 16:24:12 +0200

    1 and 3 working

--- view.py
+++ view.py
@@ -1,10 +1,13 @@
-#from Model import DbLibraryOperations
-#from controler import LibController
-#class LibViewer:
-    #def __init__(self):
-        #self.mLib = DbLibraryOperations()
-        #self.cLib = LibController()
-
-
-
+from Model import DbConnection
+from Model import DbLibraryOperations
+class LibViewer:
+    def __init__(self):
+        self.db_connection = DbConnection()
+        self.mLib = DbLibraryOperations()
+    def searchBook(self):
+        print("Zadajte ID knihy")
+        id = input()
+        search = "SELECT title FROM books WHERE book_id = %s"
+        parameter = id
+        return self.db_connection.searchQuery(search, parameter)
 
